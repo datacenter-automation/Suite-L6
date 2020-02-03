a95b9989a483860610ac380283215fbadf249fa4
Alex Renner <alexrenner@dev.fiberhop.local>
2020-02-02 21:59:45 -0600

    manual update to package.json composer.json (rec. by dependabot)

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.13.1 on 2020-02-02 18:44:51.
+ * Generated for Laravel 6.13.1 on 2020-02-02 21:57:54.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
